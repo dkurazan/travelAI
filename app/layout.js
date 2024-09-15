2f191b373c2a2e6227209c59db1cfcf038274e26
Dmytro <deekorde@gmail.com>
2024-09-15 19:46:03 +0300

    clerk api changed

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider frontendApi={process.env.CLERK_SECRET_KEY}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={inter.className}>
           <Providers>{children}</Providers>
