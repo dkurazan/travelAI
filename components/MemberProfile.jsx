85a156918a68f9f1fc2fa657372c6a2ca92b8616
unknown <deekorde@gmail.com>
2024-09-20 15:19:38 +0300

    signout url added

--- components/MemberProfile.jsx
+++ components/MemberProfile.jsx
@@ -10,7 +10,7 @@ export default async function MemberProfile() {
 
   return (
     <div className="px-4 flex items-center gap-2">
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
       <p>{user.emailAddresses[0].emailAddress}</p>
     </div>
   );
